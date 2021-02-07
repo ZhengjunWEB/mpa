940ffe91c1cd24cfdaab790ec34af94130d9a1bf
ZhengjunWEB <404459274@qq.com>
2021-02-07 11:48:09 +0800

    跳转

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,19 +1,24 @@
 <template>
-  <div>
-      <Header />
-      我是home页面
-  </div>
+    <div>
+        <Header />
+        我是home页面
+    </div>
 </template>
 
 <script>
 import Header from '@/components/header.vue'
 export default {
     components: {
-       Header 
+        Header
+    },
+    created() {
+        console.log('组件创建')
+        // console.log(window.location);
+        let search = window.location.search.slice(1)
+        console.log(search);
     }
 }
 </script>
 
 <style>
-
 </style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,7 +2,7 @@
     <div>
         我是index页面
         <div>
-            <a href="home.html">跳转到home页面</a>
+            <a href="/home?id=1&from=header">跳转到home页面</a>
             <a href="person.html">跳转到person页面</a>
         </div>
     </div>
